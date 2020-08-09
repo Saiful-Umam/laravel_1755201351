cc1d4c22a6a249106e7f06b009bc696e304d1714
Hanifah <Hanifah>
2020-08-10 04:12:48 +0700

    Pertemuan 12 Membuat Data Table

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mahasiswa;
+use DataTables;
 use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
@@ -17,6 +18,15 @@ class MahasiswaController extends Controller
         return view('mahasiswa.index');
 
     }
+
+    public function mhs_list()
+    {
+        return Datatables::of(Mahasiswa::all())
+                ->removeColumn('id')
+                ->addIndexColumn()
+                ->make(true); //Good
+    }
+    
 
     /**
      * Show the form for creating a new resource.
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -5,5 +5,34 @@
 @section('bread2', 'Daftar')
 
 @section('content')
-    <p>This is my body content.</p>
+<h2>Master Mahasiswa</h2>
+<table class="table table-striped" id ="mhs-table">
+	<thead>
+		<tr>
+			<th>No</th>
+			<th>NIM</th>
+			<th>Nama Lengkap</th>
+			<th>Program Studi</th>
+			<th>Alamat</th>
+		</tr>
+	</thead>
+	 <tbody></tbody>
+</table>
+<script>
+$(function() {
+  var table =   $('#mhs-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('mhs_list') }}",
+        columns: [
+            { data: 'DT_RowIndex', name: 'DT_RowIndex' },
+            { data: 'nim', name: 'nim' },
+            { data: 'nama_lengkap', name: 'nama_lengkap' },
+            { data: 'prodi', name: 'prodi' },
+            { data: 'alamat', name: 'alamat' }
+        ]
+    });
+});
+</script>
 @endsection
+
